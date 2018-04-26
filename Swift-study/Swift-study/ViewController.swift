88002dd7577da5e134e8d1b137c559317cddfa7c
YiQing <appdev@yiqing-group.com>
2018-04-26 15:16:17 +0800

    init project

---
+++ Swift-study/Swift-study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-study
+//
+//  Created by Yi Qing on 2018/4/26.
+//  Copyright © 2018年 Arvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
